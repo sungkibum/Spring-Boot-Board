765960364ff04fe4d7a88ca44584f9cb605392ce
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-10 17:28:44 +0900

    테스트코드에 findBySubjectLike 메서드 사용, 인터페이스에도 적용

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -10,6 +10,8 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
 
     Question findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String subject);
     //위의 메서드를 선언만 하고 구현하지 않았는데 실행이 되는 이유
     //JPA에 리포지터리의 메서드명을 분석하여 쿼리를 만들고 실행하는 기능이 있기 때문에 가능
     //findBy + 엔티티의 속성명과 같은 리포지터리의 메서드를 작성하면 입력한 속성의 값으로 데이터 조회 가능
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -18,7 +18,8 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question question = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-		assertEquals(1, question.getId());
+		List<Question> oq = questionRepository.findBySubjectLike("sbb%");
+		Question q = oq.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 		}
 	}
