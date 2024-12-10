8dd2c5833bd95d21e3713a1ae3e4e92935fbbeee
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-10 17:09:39 +0900

    테스트코드 리팩토링

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,26 @@
 package com.mysite.sbb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired	//의존성 주입(스프링이 객체를 대신 생성하여 주입하는 기법)
+	private QuestionRepository questionRepository;
 
+	@Test
+	void testJpa() {
+		List<Question> all = questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
 }
