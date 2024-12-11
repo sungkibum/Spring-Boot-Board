b7538085f9f0c37af2bf39a17451dfad28459c84
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-11 17:25:10 +0900

    질문 등록 로직 구현

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,9 +3,7 @@ package com.mysite.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +32,11 @@ public class QuestionController {
     @GetMapping("/create")
     public String questionCreate() {
         return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam(value = "subject") String subject, @RequestParam(value = "content") String content) {
+        this.questionService.create(subject, content);
+        return "redirect:/question/list";
     }
 }
