de5a10ac03027bc724d4198531dd58df69e9913f
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-11 15:02:13 +0900

    상세 페이지에 답변 표시하기

---
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,11 @@
+package com.mysite.sbb.answer;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+@Service
+public class AnswerService {
+
+}
