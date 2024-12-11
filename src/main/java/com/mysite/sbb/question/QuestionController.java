eb90aa84cb261757dd6e87175072b002562154d8
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-11 11:25:06 +0900

    질문목록 페이지 생성

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,15 +1,23 @@
 package com.mysite.sbb.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor    //final이 붙은 속성을 포함하는 생성자를 자동으로 만들어주는 역할
 @Controller
 public class QuestionController {
 
-    @ResponseBody
+    private final QuestionRepository questionRepository;
+
+//    @ResponseBody     이젠 템플릿을 사용하기 때문에 필요없음
     @GetMapping("/question/list")
-    public String questionList() {
-        return "questionList";
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
     }
 }
