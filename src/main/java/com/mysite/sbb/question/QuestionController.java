f783662e35c4cab7b28d28d8ed857562eff038ad
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-11 12:45:06 +0900

    QuestionController단에서 URL에 프리픽스 적용

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor    //final이 붙은 속성을 포함하는 생성자를 자동으로 만들어주는 역할
 @Controller
 public class QuestionController {
@@ -15,14 +17,14 @@ public class QuestionController {
     private final QuestionService questionService;
 
 //    @ResponseBody     이젠 템플릿을 사용하기 때문에 필요없음
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
-    @GetMapping("/question/detail/{id}")    //뒤에 id값은 변하는 id값이므로 PathVariable 어노테이션 사용
+    @GetMapping("/detail/{id}")    //뒤에 id값은 변하는 id값이므로 PathVariable 어노테이션 사용
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
