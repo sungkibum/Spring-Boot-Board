e464bf14e48aad34a81744736a1c957fdaedc0c5
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-12 12:19:22 +0900

    페이징 기능을 위한 대량 데이터 생성 테스트 케이스 작성

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,26 +20,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class SbbApplicationTests {
 
 	@Autowired    //의존성 주입(스프링이 객체를 대신 생성하여 주입하는 기법)
-	private AnswerRepository answerRepository;
-
-	@Autowired
-	private QuestionRepository questionRepository;
-
+	private QuestionService questionService;
 	@Transactional
 	@Test
 	void testJpa() {
-		Optional<Question> oq = questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-		List<Answer> answerList = q.getAnswerList();
-
-		//답변의 리스트의 길이를 테스트, 답변리스트의 길이가 1이므로 답변리스트의 첫번째 요소의 내용을 테스트
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
-
-		//답변 객체에 답변리스트의 첫번째 값 대입, 생성한 객체의 컬럼 중 질문으로 테스트(위에서 가져온 질문 객체(q)와
-		//답변 객체에서의 질문은 외래키이므로 같아야 한다.
-		Answer a = answerList.get(0);
-		assertEquals(q, a.getQuestion());
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[" + i + "]");
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
 	}
 }
