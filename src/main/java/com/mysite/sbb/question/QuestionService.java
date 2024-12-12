6221555129f5945b57d132f2327e17ffd7dce523
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-12 12:43:10 +0900

    게시물 최신순으로 나열

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -3,10 +3,12 @@ package com.mysite.sbb.question;
 import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,8 +18,11 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
 
@@ -39,8 +44,4 @@ public class QuestionService {
         return question;
     }
 
-    public Page<Question> getList(int page) {
-        Pageable pageable = PageRequest.of(page, 10);
-        return this.questionRepository.findAll(pageable);
-    }
 }
