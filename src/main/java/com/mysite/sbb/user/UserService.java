abc90e1797907a111ba8504bb389cd1d9de2265d
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-12 15:46:07 +0900

    비크립트 해시함수 빈으로 추가 및 리팩토링

--- src/main/java/com/mysite/sbb/user/UserService.java
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -10,12 +11,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String email, String password) {
         SiteUser siteUser = new SiteUser();
         siteUser.setUsername(username);
         siteUser.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         siteUser.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(siteUser);
         return siteUser;
