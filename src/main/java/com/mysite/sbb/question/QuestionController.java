6d4b487f81855c4579b9bb1228b6977e36ee5584
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-13 16:52:07 +0900

    질문 컨트롤러 수정

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,11 +6,13 @@ import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.List;
@@ -54,5 +56,15 @@ public class QuestionController {
         return "redirect:/question/list";
     }
 
-
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String questionModify(@PathVariable("id") Integer id, QuestionForm questionForm, Principal principal) {
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        questionForm.setContent(question.getContent());
+        questionForm.setSubject(question.getSubject());
+        return "question_form";
+    }
 }
