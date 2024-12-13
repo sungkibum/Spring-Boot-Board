66bc762e9e865924bff9cefcda0e6432b69edfad
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-13 16:01:25 +0900

    로그인이 안된 경우에는 답변 등록 막음

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -27,7 +27,7 @@ class SbbApplicationTests {
 		for (int i = 1; i <= 300; i++) {
 			String subject = String.format("테스트 데이터입니다:[" + i + "]");
 			String content = "내용무";
-			this.questionService.create(subject, content);
+			this.questionService.create(subject, content, null);
 		}
 	}
 }
